74d7cb394a6a06afa0cf102f25612dd9405e7b50
Bjarte Aune Olsen <bjarte@basementmedia.no>
2016-06-07 23:40:32 +0200

    Added code for reading metadata from more media
    Getting created date from iPhone PNG, iPhone MOV and Android MP4

--- Program.cs
+++ Program.cs
@@ -1,14 +1,25 @@
 ﻿using System;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using ExifLib;
+using MediaInfoLib;
+using TagLib;
+using TagLib.Xmp;
+using File = System.IO.File;
 
 namespace Renamer
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            const string dateFormat = "yyyy-MM-dd HH-mm-ss";
+            string[] allowedFileTypes =
+            {
+                ".jpg", ".jpeg", ".png", ".gif", ".mp4", ".avi", ".mov", ".mpg", ".mpeg"
+            };
+
             string[] filePaths = Directory.GetFiles(Directory.GetCurrentDirectory());
 
             foreach (var filePath in filePaths)
@@ -24,14 +35,14 @@ namespace Renamer
                     continue;
                 }
 
+                var fileExtension = Path.GetExtension(fileName).ToLower();
+                if (!allowedFileTypes.Contains(fileExtension))
+                {
+                    continue;
+                }
+
                 Console.WriteLine();
                 Console.WriteLine(fileName);
-
-                var fileModified = File.GetLastWriteTimeUtc(fileName);
-                Console.WriteLine("            - File modified:   " + fileModified.ToString("yyyy-MM-dd HH:mm:ss"));
-
-                var fileCreated = File.GetCreationTimeUtc(fileName);
-                Console.WriteLine("            - File created:    " + fileCreated.ToString("yyyy-MM-dd HH:mm:ss"));
 
                 // Parse file name as date
                 var fileNameDigits = new string(fileName.ToCharArray().Where(x => char.IsDigit(x)).ToArray());
@@ -46,17 +57,82 @@ namespace Renamer
                     var second = int.Parse(fileNameDigits.Substring(12, 2));
 
                     DateTime fileNameDate;
-
                     DateTime.TryParse($"{year}-{month}-{day} {hour}:{minute}:{second}", out fileNameDate);
-
                     if (fileNameDate != new DateTime())
                     {
-                        Console.WriteLine("            - Filename date:   " + fileNameDate.ToString("yyyy-MM-dd HH:mm:ss"));
+                        Console.WriteLine("               - Filename date:         " + fileNameDate.ToString(dateFormat) +
+                                          fileExtension);
+                    }
+                    else
+                    {
+                        Console.WriteLine("               - Filename date:         -");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("               - Filename date:         -");
+                }
+
+                //var fileModified = File.GetLastWriteTimeUtc(fileName);
+                //Console.WriteLine("            - File modified:      " + fileModified.ToString(dateFormat));
+
+                //var fileCreated = File.GetCreationTimeUtc(fileName);
+                //Console.WriteLine("            - File created:       " + fileCreated.ToString(dateFormat));
+
+                // Get XMP metadata with taglib
+                if (fileExtension.Equals(".png") || fileExtension.Equals(".gif"))
+                {
+                    TagLib.File file = TagLib.File.Create(fileName);
+
+                    XmpTag xmp = file.GetTag(TagTypes.XMP) as XmpTag;
+                    if (xmp != null)
+                    {
+                        var tree = xmp.NodeTree;
+                        var node = tree.GetChild(XmpTag.PHOTOSHOP_NS, "DateCreated");
+                        var dateCreated = node.Value;
+
+                        DateTime xmpDateCreated;
+                        DateTime.TryParse(dateCreated, out xmpDateCreated);
+                        if (xmpDateCreated != new DateTime())
+                        {
+                            Console.WriteLine("               - XMP DateCreated:       " + xmpDateCreated.ToString(dateFormat) + fileExtension);
+                        }
                     }
                 }
 
-                // Get date from EXIF
-                if (filePath.ToLower().EndsWith("jpg") || filePath.ToLower().EndsWith("jpeg"))
+                // Get metadata for videos with MediaInfoLib
+                if (fileExtension.Equals(".mp4") || fileExtension.Equals(".mov"))
+                {
+                    DateTime createdDate;
+
+                    // Created date for Android MP4 files and iPhone MOV files (UTC time)
+                    // Format: UTC 2016-06-07 07:07:30
+                    MediaInfo mediaInfo = new MediaInfo();
+                    mediaInfo.Open(fileName);
+                    var createdDateString = mediaInfo.Get(0, 0, "Encoded_Date");
+                    mediaInfo.Close();
+
+                    if (createdDateString.StartsWith("UTC"))
+                    {
+                        DateTime.TryParse(createdDateString.Substring(4)
+                            , CultureInfo.CurrentCulture
+                            , DateTimeStyles.AssumeUniversal
+                            , out createdDate);
+                    }
+                    else
+                    {
+                        DateTime.TryParse(createdDateString, out createdDate);
+                    }
+
+                    if (createdDate != new DateTime())
+                    {
+                        Console.WriteLine("               - Video created date:    " + createdDate.ToString(dateFormat) + fileExtension);
+                    }
+                }
+
+                // Get date from EXIF with ExifReader
+                if (fileExtension.Equals(".jpg") || fileExtension.Equals(".jpeg")
+                    )
                 {
                     using (ExifReader reader = new ExifReader(fileName))
                     {
@@ -65,8 +141,8 @@ namespace Renamer
                         if (reader.GetTagValue(ExifTags.DateTimeDigitized,
                             out datePictureTaken))
                         {
-                            Console.WriteLine("            - EXIF date:       " +
-                                              datePictureTaken.ToString("yyyy-MM-dd HH:mm:ss"));
+                            Console.WriteLine("               - EXIF date:             " +
+                                              datePictureTaken.ToString(dateFormat) + fileExtension);
                         }
                     }
                 }

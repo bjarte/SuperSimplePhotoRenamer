0bc1e62169e90f76d4ed915cddc4f70d673c4c8f
Bjarte Aune Olsen <bjarte@basementmedia.no>
2016-06-07 10:15:53 +0200

    Renamed assembly and added some information to it

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ConsoleApplication1")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("Super Simple Photo Renamer")]
+[assembly: AssemblyDescription("Renames photo/video files using the date the photo was taken")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ConsoleApplication1")]
+[assembly: AssemblyCompany("Basement Media")]
+[assembly: AssemblyProduct("Super Simple Photo Renamer")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

76406e9328b73fa4160d1ed4ed976d8aa8e540bd
Bjarte Aune Olsen <bjarte@basementmedia.no>
2016-06-08 15:46:24 +0200

    Created helpers to work with filetypes

---
+++ Helpers/FileTypeHelpers.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+
+namespace Renamer.Helpers
+{
+    public static class FileTypeHelpers
+    {
+        public static bool IsAllowedFileType(string fileType)
+        {
+            if (fileType.StartsWith("."))
+            {
+                fileType = fileType.Substring(1);
+            }
+
+            return Settings.VideoFileTypes.Contains(fileType) || Settings.ImageFileTypes.Contains(fileType);
+        }
+
+        public static bool IsVideoFileType(string fileType)
+        {
+            if (fileType.StartsWith("."))
+            {
+                fileType = fileType.Substring(1);
+            }
+
+            return Settings.VideoFileTypes.Contains(fileType);
+        }
+    }
+}

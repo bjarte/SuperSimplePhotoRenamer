68460ea25f140dfd3c0e633b8a9d8cb11dfe3bbc
Bjarte Aune Olsen <bjarte@basementmedia.no>
2016-06-08 15:47:22 +0200

    Created helpers to easier create nice layout when printing to screen

---
+++ Helpers/OutputHelpers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+
+namespace Renamer.Helpers
+{
+    public static class OutputHelpers
+    {
+        public static void WriteColumns(string[] inputs)
+        {
+            var stringBuilder = new StringBuilder();
+            foreach (var input in inputs)
+            {
+                var output = PadToColumnWidth(input);
+                stringBuilder.Append(output);
+            }
+            Console.WriteLine(stringBuilder.ToString());
+        }
+
+        private static string PadToColumnWidth(string input)
+        {
+            if (input.Length > Settings.ColumnWidth)
+            {
+                input = input.Substring(0, Settings.ColumnWidth - 3);
+                input += "...";
+            }
+
+            while (input.Length < Settings.ColumnWidth)
+            {
+                input = $" {input}";
+            }
+
+            return $"{input}{Settings.ColumnSpace}";
+        }
+    }
+}

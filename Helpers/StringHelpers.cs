320b8be3aab4f500c4a460b5c1596d7ea9e45a3e
Bjarte Aune Olsen <bjarte@basementmedia.no>
2016-06-08 15:48:35 +0200

    Created extension method to convert any string to datetime

---
+++ Helpers/StringHelpers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace Renamer.Helpers
+{
+    public static class StringHelpers
+    {
+        public static DateTime ConvertToDateTime(this string input)
+        {
+            DateTime output = new DateTime();
+
+            var inputDigits = new string(input.ToCharArray().Where(x => char.IsDigit(x)).ToArray());
+
+            if (inputDigits.Length < 12)
+            {
+                return output;
+            }
+
+            var year = int.Parse(inputDigits.Substring(0, 4));
+            var month = int.Parse(inputDigits.Substring(4, 2));
+            var day = int.Parse(inputDigits.Substring(6, 2));
+
+            var hour = int.Parse(inputDigits.Substring(8, 2));
+            var minute = int.Parse(inputDigits.Substring(10, 2));
+            var second = inputDigits.Length >= 14 ? int.Parse(inputDigits.Substring(12, 2)) : 0;
+
+            var dateString = $"{year}-{month}-{day} {hour}:{minute}:{second}";
+
+            if (input.Contains("UTC"))
+            {
+                DateTime.TryParse(dateString
+                           , CultureInfo.CurrentCulture
+                           , DateTimeStyles.AssumeUniversal
+                           , out output);
+            }
+            else
+            {
+                DateTime.TryParse(dateString, out output);
+            }
+
+            return output;
+        }
+    }
+}

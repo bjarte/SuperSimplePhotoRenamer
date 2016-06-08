0194e9a4e50ed413695ecc56c900bd8b638cb1c5
Bjarte Aune Olsen <bjarte@basementmedia.no>
2016-06-08 15:47:46 +0200

    Created settings file to keep all settings in one place

---
+++ Helpers/Settings.cs
@@ -0,0 +1,12 @@
+﻿namespace Renamer.Helpers
+{
+    public static class Settings
+    {
+        public static string DateFormat = "yyyy-MM-dd HH-mm-ss";
+        public static int ColumnWidth = 25;
+        public static string ColumnSpace = "   ";
+
+        public static string[] VideoFileTypes = { "mp4", "avi", "mov", "mpg", "mpeg" };
+        public static string[] ImageFileTypes = { "jpg", "jpeg", "png", "gif" };
+    }
+}

44394038fa891dd3820b0f77e62c0cf5305bcf1d
Bjarte Aune Olsen <bjarte@basementmedia.no>
2016-06-08 23:36:05 +0200

    The columsn are a bit wider

--- Helpers/Settings.cs
+++ Helpers/Settings.cs
@@ -3,8 +3,8 @@
     public static class Settings
     {
         public static string DateFormat = "yyyy-MM-dd HH-mm-ss";
-        public static int ColumnWidth = 25;
-        public static string ColumnSpace = "   ";
+        public static int ColumnWidth = 35;
+        public static string ColumnSpace = " ";
 
         public static string[] VideoFileTypes = { "mp4", "avi", "mov", "mpg", "mpeg" };
         public static string[] ImageFileTypes = { "jpg", "jpeg", "png", "gif" };

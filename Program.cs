7fa9af0a08603b104a0379c33d353351c465aa1e
Bjarte Aune Olsen <bjarte@basementmedia.no>
2016-06-08 15:49:29 +0200

    Started using all the helpers, moved logic out of Program.cs
    Also separated the reading of metadata and printing to screen

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿using System;
-using System.Globalization;
 using System.IO;
 using System.Linq;
 using ExifLib;
 using MediaInfoLib;
+using Renamer.Helpers;
 using TagLib;
 using TagLib.Xmp;
 using File = System.IO.File;
@@ -14,21 +14,10 @@ namespace Renamer
     {
         private static void Main(string[] args)
         {
-            const string dateFormat = "yyyy-MM-dd HH-mm-ss";
-            string[] allowedFileTypes =
-            {
-                ".jpg", ".jpeg", ".png", ".gif", ".mp4", ".avi", ".mov", ".mpg", ".mpeg"
-            };
-
             string[] filePaths = Directory.GetFiles(Directory.GetCurrentDirectory());
 
-            foreach (var filePath in filePaths)
+            foreach (var filePath in filePaths.Where(x => File.Exists(x)))
             {
-                if (!File.Exists(filePath))
-                {
-                    continue;
-                }
-
                 var fileName = Path.GetFileName(filePath);
                 if (string.IsNullOrEmpty(fileName))
                 {
@@ -36,50 +25,15 @@ namespace Renamer
                 }
 
                 var fileExtension = Path.GetExtension(fileName).ToLower();
-                if (!allowedFileTypes.Contains(fileExtension))
+                if (!FileTypeHelpers.IsAllowedFileType(fileExtension))
                 {
                     continue;
                 }
 
-                Console.WriteLine();
-                Console.WriteLine(fileName);
-
                 // Parse file name as date
-                var fileNameDigits = new string(fileName.ToCharArray().Where(x => char.IsDigit(x)).ToArray());
-                if (fileNameDigits.Length >= 14)
-                {
-                    var year = int.Parse(fileNameDigits.Substring(0, 4));
-                    var month = int.Parse(fileNameDigits.Substring(4, 2));
-                    var day = int.Parse(fileNameDigits.Substring(6, 2));
-
-                    var hour = int.Parse(fileNameDigits.Substring(8, 2));
-                    var minute = int.Parse(fileNameDigits.Substring(10, 2));
-                    var second = int.Parse(fileNameDigits.Substring(12, 2));
-
-                    DateTime fileNameDate;
-                    DateTime.TryParse($"{year}-{month}-{day} {hour}:{minute}:{second}", out fileNameDate);
-                    if (fileNameDate != new DateTime())
-                    {
-                        Console.WriteLine("               - Filename date:         " + fileNameDate.ToString(dateFormat) +
-                                          fileExtension);
-                    }
-                    else
-                    {
-                        Console.WriteLine("               - Filename date:         -");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine("               - Filename date:         -");
-                }
-
-                //var fileModified = File.GetLastWriteTimeUtc(fileName);
-                //Console.WriteLine("            - File modified:      " + fileModified.ToString(dateFormat));
-
-                //var fileCreated = File.GetCreationTimeUtc(fileName);
-                //Console.WriteLine("            - File created:       " + fileCreated.ToString(dateFormat));
 
                 // Get XMP metadata with taglib
+                DateTime xmpDateCreated = new DateTime();
                 if (fileExtension.Equals(".png") || fileExtension.Equals(".gif"))
                 {
                     TagLib.File file = TagLib.File.Create(fileName);
@@ -91,61 +45,86 @@ namespace Renamer
                         var node = tree.GetChild(XmpTag.PHOTOSHOP_NS, "DateCreated");
                         var dateCreated = node.Value;
 
-                        DateTime xmpDateCreated;
                         DateTime.TryParse(dateCreated, out xmpDateCreated);
-                        if (xmpDateCreated != new DateTime())
-                        {
-                            Console.WriteLine("               - XMP DateCreated:       " + xmpDateCreated.ToString(dateFormat) + fileExtension);
-                        }
+
                     }
                 }
 
                 // Get metadata for videos with MediaInfoLib
-                if (fileExtension.Equals(".mp4") || fileExtension.Equals(".mov"))
+                DateTime mediaInfoEncodedDate = new DateTime();
+                DateTime mediaInfoMasteredDate = new DateTime();
+                if (FileTypeHelpers.IsVideoFileType(fileExtension))
                 {
-                    DateTime createdDate;
+                    MediaInfo mediaInfo = new MediaInfo();
+                    mediaInfo.Open(fileName);
 
                     // Created date for Android MP4 files and iPhone MOV files (UTC time)
                     // Format: UTC 2016-06-07 07:07:30
-                    MediaInfo mediaInfo = new MediaInfo();
-                    mediaInfo.Open(fileName);
-                    var createdDateString = mediaInfo.Get(0, 0, "Encoded_Date");
+                    var encodedDateString = mediaInfo.Get(0, 0, "Encoded_Date");
+                    mediaInfoEncodedDate = encodedDateString.ConvertToDateTime();
+
+                    var masteredDateString = mediaInfo.Get(0, 0, "Mastered_Date");
+                    mediaInfoMasteredDate = masteredDateString.ConvertToDateTime();
+
                     mediaInfo.Close();
-
-                    if (createdDateString.StartsWith("UTC"))
-                    {
-                        DateTime.TryParse(createdDateString.Substring(4)
-                            , CultureInfo.CurrentCulture
-                            , DateTimeStyles.AssumeUniversal
-                            , out createdDate);
-                    }
-                    else
-                    {
-                        DateTime.TryParse(createdDateString, out createdDate);
-                    }
-
-                    if (createdDate != new DateTime())
-                    {
-                        Console.WriteLine("               - Video created date:    " + createdDate.ToString(dateFormat) + fileExtension);
-                    }
                 }
 
                 // Get date from EXIF with ExifReader
-                if (fileExtension.Equals(".jpg") || fileExtension.Equals(".jpeg")
-                    )
+                DateTime exifDateDigitized = new DateTime();
+                if (fileExtension.Equals(".jpg") || fileExtension.Equals(".jpeg"))
                 {
                     using (ExifReader reader = new ExifReader(fileName))
                     {
-                        // Extract the tag data using the ExifTags enumeration
-                        DateTime datePictureTaken;
-                        if (reader.GetTagValue(ExifTags.DateTimeDigitized,
-                            out datePictureTaken))
-                        {
-                            Console.WriteLine("               - EXIF date:             " +
-                                              datePictureTaken.ToString(dateFormat) + fileExtension);
-                        }
+                        reader.GetTagValue(ExifTags.DateTimeDigitized, out exifDateDigitized);
                     }
                 }
+
+                // Print all results
+                Console.WriteLine();
+
+                // Print filename and date from filename
+                var fileNameDate = fileName.ConvertToDateTime();
+                if (fileNameDate != new DateTime())
+                {
+                    OutputHelpers.WriteColumns(new[]
+                    { fileName, "Date from filename:", fileNameDate.ToString(Settings.DateFormat) });
+                }
+                else
+                {
+                    OutputHelpers.WriteColumns(new[] { fileName, "Date from filename:", "---" });
+                }
+
+                // Print date found in XMP DateCreated property
+                if (xmpDateCreated != new DateTime())
+                {
+                    OutputHelpers.WriteColumns(new[] { "", "XMP DateCreated:", xmpDateCreated.ToString(Settings.DateFormat) });
+                }
+
+                // Print date found in MediaInfo Encoded_Date property
+                if (mediaInfoEncodedDate != new DateTime())
+                {
+                    OutputHelpers.WriteColumns(new[] { "", "MediaInfo Encoded_Date:", mediaInfoEncodedDate.ToString(Settings.DateFormat) });
+                }
+
+                // Print date found in MediaInfo Mastered_Date property
+                if (mediaInfoMasteredDate != new DateTime())
+                {
+                    OutputHelpers.WriteColumns(new[] { "", "MediaInfo Mastered_Date:", mediaInfoMasteredDate.ToString(Settings.DateFormat) });
+                }
+
+                // Print date found in EXIF Date Digitized property
+                if (exifDateDigitized != new DateTime())
+                {
+                    OutputHelpers.WriteColumns(new[] { "", "EXIF DateDigitized:", exifDateDigitized.ToString(Settings.DateFormat) });
+                }
+
+                // Print date found in File modified property
+                //var fileModified = File.GetLastWriteTime(fileName);
+                //OutputHelpers.WriteColumns(new[] { "", "File modified:", fileModified.ToString(Settings.DateFormat) });
+
+                // Print date found in File created property
+                //var fileCreated = File.GetCreationTime(fileName);
+                //OutputHelpers.WriteColumns(new[] { "", "File created:", fileCreated.ToString(Settings.DateFormat) });
             }
         }
     }

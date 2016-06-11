6d685f213809cb9813f02409c101dec86e6c1462
Bjarte Aune Olsen <bjarte@basementmedia.no>
2016-06-11 17:34:52 +0200

    Created a model for a mediafile to keep all the possible dates from mediadata

---
+++ Models/MediaData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Renamer.Models
+{
+    public class MediaData
+    {
+        public string Path { get; set; }
+
+        public string FileName { get; set; }
+        public string Extension { get; set; }
+
+        public DateTime XmpDateCreated { get; set; }
+        public DateTime MediaInfoEncodedDate { get; set; }
+        public DateTime MediaInfoMasteredDate { get; set; }
+        public DateTime ExifDateDigitized { get; set; }
+        public DateTime ExifDateOriginal { get; set; }
+
+        public DateSource SourceForFilenameDate { get; set; }
+    }
+
+    public enum DateSource
+    {
+        Original,
+        FromFilename,
+        XmpDateCreated,
+        MediaInfoEncodedDate,
+        MediaInfoMasteredDate,
+        ExifDateDigitized,
+        ExifDateOriginal
+    }
+}
+
+
